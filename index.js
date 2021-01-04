0c7abe0c1d332bd1b07c6540f074e441172cdb39
satsuger <satsuger@gmail.com>
2021-01-05 00:48:46 +0200

    basic layout

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
+const exphbs = require("express-handlebars");
 const express = require("express");
 const path = require("path");
+const { ppid } = require("process");
+
+// Configure
 const app = express();
 
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+// Middlevares
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.render("index", {
+    title: 'Home page',
+    isHome: true
+  });
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "about.html"));
+app.get("/add", (req, res) => {
+  res.render("add", {
+    title: 'Add daun page',
+    isAdd: true
+  });
 });
 
+app.get("/dauns", (req, res) => {
+  res.render("dauns", {
+    title: 'Dauns page',
+    isDauns: true
+  });
+});
+
+// Listen
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
